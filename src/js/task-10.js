3db42411aa50672fd1e8582f8bf4c174823061a9
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-09-19 00:36:39 +0300

    M6 T10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const boxes = [];
+  let boxSize = 30;
+for (let i = 0; i < amount; i += 1) {
+  const box = document.createElement('div');
+  box.style.width = `${boxSize}px`;
+  box.style.height = `${boxSize}px`;
+  box.style.backgroundColor = getRandomHexColor();
+boxes.push(box);
+  boxSize += 10;
+}
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
